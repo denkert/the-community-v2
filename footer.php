7c7203c75a101e172bda53b0cc4ebe75e757f2c6
Ellinor Denkert <ellinor.denkert@me.com>
2015-08-14 18:10:05 -0400

    fixing a few kinks with the mobile. added more styling to footer.

--- footer.php
+++ footer.php
@@ -15,12 +15,12 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			We would like to thank our generous donors: Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson
+			<p>We would like to thank our generous donors: Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson, Name Namesson
 			<br><br>The Community 2015 
 			<span class="sep"> | </span>
  			All Rights Reserved
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'communityv2' ), 'Community 2.0', '<a href="http://ellinordenkert.com" rel="designer">Ellinor Denkert</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'communityv2' ), 'Community 2.0', '<a href="http://ellinordenkert.com" rel="designer">Ellinor Denkert</a>' ); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
